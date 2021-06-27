195c2da66bbc259a9c52ee98ba136670166e9f23
Marc Backes <hello@marc.dev>
2021-06-27 15:26:13 +0200

    created stage 3

--- reactivity.js
+++ reactivity.js
@@ -5,19 +5,58 @@
 
 const product = { price: 15, quantity: 2 }
 
-let total = product.price * product.quantity
+let total = 0
 
-console.log({ total })
+const calculateTotal = () => {
+    total = product.price * product.quantity
+}
 
-product.quantity = 3
-console.log({ total }) // Does not recalculate
+let dep = new Set() // list of effects will be saved here
 
-product.price = 12
-console.log({ total }) // Does not recalculate
+// Register an effect
+function track(target, key) {
+    // target and key are not used yet.
+    // they will come into play in stage 5
 
-// executing total = product.price * product.quantity
-// every time something changes would be tedious 😣
+    // Add effect
+    dep.add(calculateTotal)
+}
 
-product.price = 20
-total = product.price * product.quantity
-console.log({ total }) // Recalculate due to line 22
+// Execute all registered effects for the target/key combination
+function trigger(target, key) {
+    // target and key are not used yet.
+    // they will come into play in stage 5
+
+    // Execute all effects
+    dep.forEach(effect => effect())
+}
+
+const proxy = new Proxy(product, {
+    // Intercept getter
+    get(target, key, receiver) {
+        console.log('get', key, 'from', target)
+        const result = Reflect.get(target, key, receiver)
+        track(target, key) //track changes for the key in the target
+        return result
+    },
+    // Intercept setter
+    set(target, key, value, receiver) {
+        console.log('set', key, '=>', value)
+        const result = Reflect.set(target, key, value, receiver)
+        trigger(target, key) // trigger a change in the target
+        return result
+    },
+})
+
+console.log(proxy.price)
+proxy.price = 25
+console.log(proxy.price)
+proxy.quantity = 5
+console.log(proxy.quantity)
+
+// Results in:
+// get price from {price: 15, quantity: 2}
+// set price => 25
+// get price from {price: 25, quantity: 2}
+// set quantity => 5
+// get quantity from {price: 25, quantity: 5}
