bea0b541fa8e4386d37c1793f368e4a0a798fe3c
Marc Backes <hello@marc.dev>
2021-06-27 15:11:41 +0200

    created stage 1

--- reactivity.js
+++ reactivity.js
@@ -3,4 +3,21 @@
 // Marc Backes (@themarcba)
 // ----------------------------------------------------------------
 
-console.log('hello devs 👋')
+const product = { price: 15, quantity: 2 }
+
+let total = product.price * product.quantity
+
+console.log({ total })
+
+product.quantity = 3
+console.log({ total }) // Does not recalculate
+
+product.price = 12
+console.log({ total }) // Does not recalculate
+
+// executing total = product.price * product.quantity
+// every time something changes would be tedious 😣
+
+product.price = 20
+total = product.price * product.quantity
+console.log({ total }) // Recalculate due to line 22
